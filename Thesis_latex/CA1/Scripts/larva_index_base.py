ea5b282e35bfd70174d188af1398f454afe0c2fe
ElY0da <51644867+ElY0da@users.noreply.github.com>
2020-04-27 23:09:30 +0530

    Thesis_ackn

--- Thesis_latex/CA1/Scripts/larva_index_base.py
+++ Thesis_latex/CA1/Scripts/larva_index_base.py
@@ -1,29 +1,83 @@
-import numpy as np
+import numpy as np # importing numpy module
 
+#larval growth function runs larval growth function and pupal mortality
+# lfood = larval food amout, m_index, f_index = array of numbered males and females respectively for indexing
+# m_traits, f_traits = dictionary containing trait values of all males and females repectively for each trait   
+def larval_growth(lfood, m_index, f_index, m_traits, f_traits):
+    nf = len(f_index)   #number of females in one vial
+    nm = len(m_index)   #number of males in one vials
+    #defining dictionaries containing variables for male and female
+    # size = body size, dt = time to reach critical size, frt = feeding rate at critical size
+    # w_acum = waste accumulated in the body during larval feeding
+    # food_aq = food aquired by each larva during 'dt' time step
+    # waste_prod = waste produced by each larva during 'dt' time step
+    m_var = dict(size = np.repeat(3., nm), dt = np.zeros(nm), frt = np.zeros(nm), w_acum = np.zeros(nm), food_aq = np.zeros(nm), waste_prod = np.zeros(nm))
+    f_var = dict(size = np.repeat(3., nf), dt = np.zeros(nf), frt = np.zeros(nf), w_acum = np.zeros(nf), food_aq = np.zeros(nf), waste_prod = np.zeros(nf))
+    # sexually dimporhic trait assigned here:
+    feff, meff = f_traits['eff']*0.011, m_traits['eff']*0.009
+    fmc, mmc = f_traits['mc']*1.1, m_traits['mc']*.9
+    dt = 30     # time step
+    fQ = dict(fband = 1., dband = 1.)   # food quality of feeding and diffusion band
+    waste = dict(fband = 0, dband = 0)  # waste buil-up in feeding and diffusion band
+    total_waste = 0     # amount of waste in the total food
+    kd = 0.002*dt   # proportion of waste diffusion
+    
+    # for loop for discrete larval growth
+    for t in np.arange(0,105*60,dt):
+        # if amount of waste exceeds food amount, larval growth stops
+        if lfood <= total_waste:
+            break
+        # else iterate function changes variables and body size 
+        fout = iterate(t, dt, nf, lfood, f_traits['fr'], f_traits['wtol'], feff, fmc, total_waste, f_index, fQ['fband'], f_var)
+        mout = iterate(t, dt, nm, lfood, m_traits['fr'], m_traits['wtol'], meff, mmc, total_waste, m_index,fQ['fband'], m_var)  
+        # at each time step, following variables are updated:
+        food_aq = sum(f_var['food_aq']) + sum(m_var['food_aq'])
+        waste_prod = sum(f_var['waste_prod']) + sum(m_var['waste_prod'])
+        lfood -= (food_aq - waste_prod)
+        # foodQ function updates food quality variable at each time step
+        foodQ(kd, lfood, food_aq, waste_prod, fQ, waste)
+        total_waste += waste_prod
+    
+    #survive function is for pupal mortality dependent on waste accumulated:    
+    falive_out = survive(fout[1], fmc, f_var)
+    malive_out = survive(mout[1], mmc, m_var)
+    
+    # survivorship, body size, time to reach critical size, final feeding rate and index number of all surviving individuals are stored in the following arrays: 
+    survivors = (nf-sum(falive_out[0]) + nm-sum(malive_out[0]))*.98/(nf+nm)
+    alive_size = dict(female = falive_out[1], male = malive_out[1])
+    alive_devt = dict(female = falive_out[2], male = malive_out[2])
+    alive_index = dict(female = falive_out[3], male = malive_out[3])
+    alive_frt = dict(female = falive_out[4], male = malive_out[4])
+    
+    #output the above arrays
+    return alive_size, alive_devt, alive_frt, survivors, alive_index
+
+# iterate function runs at each time step till time 't' 
 def iterate(t, dt, n, lfood, frc, wtol, eff, mc, total_waste, larvae, fQ, var):
-    np.random.shuffle(larvae)
-    vf_pre, vf_post = 1, 1.5
-    u = 10**4
-    food_aq = var['food_aq']
+    np.random.shuffle(larvae)   # shuffle larvae for body size increment
+    vf_pre, vf_post = 1, 1.5    # volume of food eaten in one time step (pre and post critical)
+    u = 10**4   #scaling parameter
+    food_aq = var['food_aq']    
     waste_prod = var['waste_prod']
-
+    
+    # for loop for each larva
     for i in larvae:
-        if lfood > total_waste and fQ > 0 :
-            if var['size'][i] < mc[i]:
-                if var['size'][i] != -1 :
-                    var['frt'][i] = frc[i] + 0.017 * t
-                    vfood = vf_pre * dt
-                    incr = var['frt'][i] * vfood * fQ * eff[i]
-                    if var['w_acum'][i] > u * wtol[i]:
-                        var['size'][i] = -1
+        if lfood > total_waste and fQ > 0 : # availability of edible food
+            if var['size'][i] < mc[i]:      # pre-critical size condition
+                if var['size'][i] != -1 :   # condition for not dead
+                    var['frt'][i] = frc[i] + 0.017 * t  # increase in the feeding rate at time 't'
+                    vfood = vf_pre * dt     # volume of food eaten in 'dt' time step
+                    incr = var['frt'][i] * vfood * fQ * eff[i]  # increase in the body size
+                    if var['w_acum'][i] > u * wtol[i]:  # condtion for waste tolerance
+                        var['size'][i] = -1             # if not tolerant then dead
                     else:
-                        var['size'][i] += incr
-                        var['w_acum'][i] +=  var['frt'][i] * vfood * (1 - fQ)
-                        waste_prod[i] = var['frt'][i] * vfood * fQ * (1 - eff[i])
-                        food_aq[i] = var['frt'][i] * vfood
+                        var['size'][i] += incr          # else body size increased
+                        var['w_acum'][i] +=  var['frt'][i] * vfood * (1 - fQ) # waste accumulted
+                        waste_prod[i] = var['frt'][i] * vfood * fQ * (1 - eff[i]) # waste produced
+                        food_aq[i] = var['frt'][i] * vfood  # food consumed
                         var['dt'][i] += dt
-            else:
-                if t < var['dt'][i] + 48*60:
+            else:   # post-critical condtions
+                if t < var['dt'][i] + 48*60: # larval feeding only till 48*60 after critical size is reached
                     vfood = vf_post * dt
                     incr = var['frt'][i] * vfood * fQ * eff[i]
                     if var['w_acum'][i] <= u * wtol[i]:
@@ -33,11 +87,12 @@ def iterate(t, dt, n, lfood, frc, wtol, eff, mc, total_waste, larvae, fQ, var):
                         food_aq[i] = var['frt'][i] * vfood
                 else:
                     break
-        elif var['size'][i] < mc[i]:
+        elif var['size'][i] < mc[i]: # death if critical size is not reached before food becomes unedible
             var['size'][i] = -1
-
+    
     return var, larvae
 
+# food quality change conditions are given in the following function:
 def foodQ(kd, lfood, food_aq, waste_prod, fQ, waste):
     fband = 50*4*(35.5*10**5)/8
     dband = fband
@@ -56,8 +111,9 @@ def foodQ(kd, lfood, food_aq, waste_prod, fQ, waste):
 
     return fQ, waste
 
-def survive(index, mc, wtol, var):
-    u = 0.000007
+# pupal mortality condtions:
+def survive(index, mc, var):
+    u = 0.000007 #scaling parameter
     ldead = np.zeros(2)
     alive_size = []
     alive_devt = []
@@ -74,41 +130,3 @@ def survive(index, mc, wtol, var):
             alive_index += [i]
             alive_fr += [var['frt'][i]]
     return ldead, alive_size, alive_devt, alive_index, alive_fr
-
-def larval_growth(lfood, m_index, f_index, m_traits, f_traits):
-    nf = len(f_index)
-    nm = len(m_index)
-    m_var = dict(size = np.repeat(3., nm), dt = np.zeros(nm), frt = np.zeros(nm), w_acum = np.zeros(nm), food_aq = np.zeros(nm), waste_prod = np.zeros(nm))
-    f_var = dict(size = np.repeat(3., nf), dt = np.zeros(nf), frt = np.zeros(nf), w_acum = np.zeros(nf), food_aq = np.zeros(nf), waste_prod = np.zeros(nf))
-    feff, meff = f_traits['eff']*0.011, m_traits['eff']*0.009
-    fmc, mmc = f_traits['mc']*1.1, m_traits['mc']*.9
-    dt = 30
-    fQ = dict(fband = 1., dband = 1.)
-    waste = dict(fband = 0, dband = 0)
-    total_waste = 0
-    kd = 0.002*dt
-    lt, lgraph = [], []
-    for t in np.arange(0,105*60,dt):
-        if lfood <= total_waste:
-            break
-        fout = iterate(t, dt, nf, lfood, f_traits['fr'], f_traits['wtol'], feff, fmc, total_waste, f_index, fQ['fband'], f_var)
-        mout = iterate(t, dt, nm, lfood, m_traits['fr'], m_traits['wtol'], meff, mmc, total_waste, m_index,fQ['fband'], m_var)
-        food_aq = sum(f_var['food_aq']) + sum(m_var['food_aq'])
-        waste_prod = sum(f_var['waste_prod']) + sum(m_var['waste_prod'])
-        lfood -= (food_aq - waste_prod)
-        foodQ(kd, lfood, food_aq, waste_prod, fQ, waste)
-        total_waste += waste_prod
-
-        lt += [t/60]
-        lgraph += [fQ['fband']]
-
-    falive_out = survive(fout[1], fmc, f_traits['wtol'], f_var)
-    malive_out = survive(mout[1], mmc, m_traits['wtol'], m_var)
-
-    survivors = (nf-sum(falive_out[0]) + nm-sum(malive_out[0]))*.98/(nf+nm)
-    alive_size = dict(female = falive_out[1], male = malive_out[1])
-    alive_devt = dict(female = falive_out[2], male = malive_out[2])
-    alive_index = dict(female = falive_out[3], male = malive_out[3])
-    alive_frt = dict(female = falive_out[4], male = malive_out[4])
-    
-    return alive_size, alive_devt, alive_frt, survivors, alive_index, lt, lgraph
--- Thesis_latex/CA1/Scripts/timeseries.py
+++ Thesis_latex/CA1/Scripts/timeseries.py
@@ -1,21 +1,87 @@
-import numpy as np
-import multiprocessing as multi
+# importing numpy and larval stage module
+import numpy as np 
 import larva_index_base as lfun
+import multiprocessing as multi # stable in linux (otherwise run larval stage module in for loop)
 
+# timeseries function runs evolution of larval trait parameters.
+# gen = number of generations, eggs = number of eggs, f = food level multiplier, rep = replicates
+# par_out = output dictionary for trait parameters
+# lhpar = dictionary for initial mean values of each trait
+# lhstd = dictionary for initial standard deviation values of each trait
+# hrt = dictionary containing values of standard deviation in mpv of each trait
+def timeseries(gen, eggs, f, rep, par_out, lhpar, lhstd, hrt):
+    nut = 1.49          # adult nutrition  
+    pop_size = 2000     #adult population size
+    # assigning number of vials based on number of eggs
+    if eggs == 60:      
+        vials = 40
+    if eggs == 600:
+        vials = 24
+    if eggs == 1200:
+        vials = 12
+    lfood  = 50*f*(37*10**5)           # larval food
+    xpar = dict(x5 = 15.0, x6 = 1.0)   # scaling parameters     
+    nlarva = int(lhpar['ht'] * eggs)   # number of larvae (ht: hatchetability)
+    nf = nlarva // 2    # number of female larvae
+    nm = nlarva - nf# number of male larvae
+    # assign indexes and trait distributions for females and males:
+    index = dict(female = np.arange(nf*vials), male = np.arange(nm*vials))
+    m_traits = dict(fr = np.random.normal(lhpar['fr'], lhstd['fr'], nm*vials), wtol = np.random.normal(lhpar['wtol'], lhstd['wtol'], nm*vials), eff = np.random.normal(lhpar['eff'], lhstd['eff'], nm*vials), mc = np.random.normal(lhpar['mc']*10**5, lhstd['mc'], nm*vials))
+    f_traits = dict(fr = np.random.normal(lhpar['fr'], 1, nf*vials), wtol = np.random.normal(lhpar['wtol'], 1., nf*vials), eff = np.random.normal(lhpar['eff'], 1., nf*vials), mc = np.random.normal(lhpar['mc']*10**5, 7500., nf*vials))
+    
+    # start the timeseries:
+    for t in range(gen):
+        p = multi.Pool(8) # assiging number of cores for multi-proccessing
+        # larval grwoth output for given number of vials:
+        larval_out = p.starmap(lfun.larval_growth, [(lfood, index['male'][z*nm:(z+1)*nm], index['female'][z*nf:(z+1)*nf], m_traits, f_traits) for z in range(vials)])
+        p.close()
+        
+        # index outpur of larval_out : 0 = size; 1 = dt; 2 = index; 3 = frt; 4 = surv
+        fi_alive = np.concatenate([larval_out[z][4]['female'] for z in range(vials)])
+        mi_alive = np.concatenate([larval_out[z][4]['male'] for z in range(vials)])
+        
+        # arranging the array for alive individuals:
+        f_index, m_index = np.arange(len(fi_alive)), np.arange(len(mi_alive))
+        np.random.shuffle(f_index), np.random.shuffle(m_index)
+        
+        fi_adults = np.array([fi_alive[i] for i in f_index][:pop_size//2])
+        mi_adults = np.array([mi_alive[i] for i in m_index][:pop_size//2])
+        
+        lf_size = np.concatenate([larval_out[z][0]['female'] for z in range(vials)])
+        f_size = np.array([lf_size[i] for i in f_index][:pop_size//2])
+        
+        # store output of all trait values:
+        fr = [f_traits['fr'][i] for i in fi_adults]+[m_traits['fr'][i] for i in mi_adults]
+        wtol = [f_traits['wtol'][i] for i in fi_adults]+[m_traits['wtol'][i] for i in mi_adults]
+        eff = [f_traits['eff'][i] for i in fi_adults]+[m_traits['eff'][i] for i in mi_adults]
+        mc = [f_traits['mc'][i] for i in fi_adults]+[m_traits['mc'][i] for i in mi_adults]
+        par_out['fr'][t] = [t, np.average(fr), np.std(fr)/np.sqrt(pop_size), pop_size, rep]
+        par_out['wtol'][t] = [t, np.average(wtol), np.std(wtol)/np.sqrt(pop_size), pop_size, rep]
+        par_out['eff'][t] = [t, np.average(eff), np.std(eff)/np.sqrt(pop_size), pop_size, rep]
+        par_out['mc'][t] = [t, np.average(mc), np.std(mc)/np.sqrt(pop_size), pop_size, rep]
+        
+        # fecundity function to obtain number of eggs per female:
+        norm_fec = fecundity(nut, xpar, lhpar['sen_siz'], f_size, vials, eggs)
+         
+        # assign traits for each offspring using inertiance function:
+        offs_traits = inherit(fi_adults, mi_adults, norm_fec, f_traits, m_traits)
+        
+        # sort offspring into males and females for next generation:
+        f_traits = dict(fr = offs_traits['fr'][:nf*vials], wtol = offs_traits['wtol'][:nf*vials], eff = offs_traits['eff'][:nf*vials], mc = offs_traits['mc'][:nf*vials])
+        m_traits = dict(fr = offs_traits['fr'][nf*vials:], wtol = offs_traits['wtol'][nf*vials:], eff = offs_traits['eff'][nf*vials:], mc = offs_traits['mc'][nf*vials:])
+        
+    p.join()   
+    # output of timeseries:
+    return par_out
+
+#fecundity function gives number of offspring per mated female
 def fecundity(nut, xpar, sen_siz, a_size, vials, eggs):
     tot_fec = nut * xpar['x5'] * np.log(xpar['x6'] + sen_siz * a_size)
-    if sum(tot_fec) < eggs*vials:
-        vials = sum(tot_fec)//eggs
-    while sum(tot_fec) > eggs*vials and vials < 12:
-            vials += 1
     prob = [i/sum(tot_fec) for i in tot_fec]
     norm_fec = np.random.multinomial(vials*eggs,prob)
-    return norm_fec, vials
+    return norm_fec
 
-def mpvalue(male, female, sd):
-    mean = (male+female)/2
-    return np.random.normal(mean,mean*sd)
-
+# inherit function to sort offspring traits:
 def inherit(fi_adults, mi_adults, norm_fec, f_traits, m_traits):
     mother_offs = []
     noffs = sum(norm_fec) 
@@ -36,69 +102,7 @@ def inherit(fi_adults, mi_adults, norm_fec, f_traits, m_traits):
     np.random.shuffle(offs_traits['mc'])
     return offs_traits
 
-def timeseries(gen, eggs, f, rep, par_out, lhpar, lhstd, hrt):
-    nut = 1.49
-    pop_size = 2000
-    if eggs == 60:
-        vials = 40
-    if eggs == 600:
-        vials = 24
-    if eggs == 1200:
-        vials = 12
-    lfood  = 50*f*(37*10**5)
-    xpar = dict(x5 = 15.0, x6 = 1.0)
-    nlarva = int(lhpar['ht'] * eggs)
-    nf = nlarva // 2
-    nm = nlarva - nf
-    index = dict(female = np.arange(nf*vials), male = np.arange(nm*vials))
-    m_traits = dict(fr = np.random.normal(lhpar['fr'], lhstd['fr'], nm*vials), wtol = np.random.normal(lhpar['wtol'], lhstd['wtol'], nm*vials), eff = np.random.normal(lhpar['eff'], lhstd['eff'], nm*vials), mc = np.random.normal(lhpar['mc']*10**5, lhstd['mc'], nm*vials))
-    f_traits = dict(fr = np.random.normal(lhpar['fr'], 1, nf*vials), wtol = np.random.normal(lhpar['wtol'], 1., nf*vials), eff = np.random.normal(lhpar['eff'], 1., nf*vials), mc = np.random.normal(lhpar['mc']*10**5, 7500., nf*vials))
-    m_var = dict(size = np.repeat(3., nm*vials), dt = np.zeros(nm*vials), frt = np.zeros(nm*vials), w_acum = np.zeros(nm*vials), food_aq = np.zeros(nm*vials), waste_prod = np.zeros(nm*vials))
-    f_var = dict(size = np.repeat(3., nf*vials), dt = np.zeros(nf*vials), frt = np.zeros(nf*vials), w_acum = np.zeros(nf*vials), food_aq = np.zeros(nf*vials), waste_prod = np.zeros(nf*vials))
-    
-    #index for output list: 0 = mean, 1 = std_err 
-    
-    for t in range(gen):
-        p = multi.Pool(8)
-        larval_out = p.starmap(lfun.larval_growth, [(lfood, index['male'][z*nm:(z+1)*nm], index['female'][z*nf:(z+1)*nf], m_var, f_var, m_traits, f_traits) for z in range(vials)])
-        p.close()
-        
-        # index outpur of larval_out : 0 = size; 1 = dt; 2 = index; 3 = frt; 4 = surv
-        fi_alive = np.concatenate([larval_out[z][4]['female'] for z in range(vials)])
-        mi_alive = np.concatenate([larval_out[z][4]['male'] for z in range(vials)])
-        
-        f_index, m_index = np.arange(len(fi_alive)), np.arange(len(mi_alive))
-        np.random.shuffle(f_index), np.random.shuffle(m_index)
-        
-        fi_adults = np.array([fi_alive[i] for i in f_index][:pop_size//2])
-        mi_adults = np.array([mi_alive[i] for i in m_index][:pop_size//2])
-        
-        lf_size = np.concatenate([larval_out[z][0]['female'] for z in range(vials)])
-        f_size = np.array([lf_size[i] for i in f_index][:pop_size//2])
-        
-        # output:
-   
-       
-        fr = [f_traits['fr'][i] for i in fi_adults]+[m_traits['fr'][i] for i in mi_adults]
-        wtol = [f_traits['wtol'][i] for i in fi_adults]+[m_traits['wtol'][i] for i in mi_adults]
-        eff = [f_traits['eff'][i] for i in fi_adults]+[m_traits['eff'][i] for i in mi_adults]
-        mc = [f_traits['mc'][i] for i in fi_adults]+[m_traits['mc'][i] for i in mi_adults]
-        par_out['fr'][t] = [t, np.average(fr), np.std(fr)/np.sqrt(pop_size), pop_size, rep]
-        par_out['wtol'][t] = [t, np.average(wtol), np.std(wtol)/np.sqrt(pop_size), pop_size, rep]
-        par_out['eff'][t] = [t, np.average(eff), np.std(eff)/np.sqrt(pop_size), pop_size, rep]
-        par_out['mc'][t] = [t, np.average(mc), np.std(mc)/np.sqrt(pop_size), pop_size, rep]
-        
-        # fecundity:
-        fec_out = fecundity(nut, xpar, lhpar['sen_siz'], f_size, vials, eggs)
-        norm_fec, vials = fec_out[0], fec_out[1]
-        
-        if vials == 0:
-            print('popluation crashed :(')
-            break
-        
-        offs_traits = inherit(fi_adults, mi_adults, norm_fec, f_traits, m_traits)
-        f_traits = dict(fr = offs_traits['fr'][:nf*vials], wtol = offs_traits['wtol'][:nf*vials], eff = offs_traits['eff'][:nf*vials], mc = offs_traits['mc'][:nf*vials])
-        m_traits = dict(fr = offs_traits['fr'][nf*vials:], wtol = offs_traits['wtol'][nf*vials:], eff = offs_traits['eff'][nf*vials:], mc = offs_traits['mc'][nf*vials:])
-        print('gen =',t, ';',eggs, 'eggs /', f*1.5, 'ml ; vials =', vials, '; rep =', rep)
-    p.join()   
-    return par_out
+#mid-parent value (mpv) function:
+def mpvalue(male, female, sd):
+    mean = (male+female)/2
+    return np.random.normal(mean,mean*sd)
